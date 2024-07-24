e27754d465e050595cc81885f0a789a862fefd81
cemedisball <cemedisbod@gmail.com>
2024-07-24 10:06:43 +0700

    first commit

--- routes/Read.js
+++ routes/Read.js
@@ -17,4 +17,20 @@ readRoute.get('/orders',function(res,req,next){
 readRoute.use('/',function(req,res,next){
     res.sendStatus(404);
 });
+readRoute.get('/orders',function(res,req,next){
+    connection.execute('SELECT * FROM orders WHERE  order_id=?;'
+        [req.params.order_id]
+    )
+    .then((result) => {
+        var rawData = result[0];
+        res.send(rawData);
+    }).catch((err) => {
+    console.log(err);
+    res.end();
+});
+});
+readRoute.use('/',function(req,res,next){
+    res.sendStatus(404);
+});
+module.exports = readRoute;
 module.exports = readRoute;
